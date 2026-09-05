82da79ee8b49bf617a7e7d2f387cb0491d6e23c3
Silvana-O <silvanaottonelli@gmail.com>
2026-09-04 21:37:54 -0300

    Actualizar precios a dólares

--- js/catalogo.js
+++ js/catalogo.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         </p>
 
                         <p class="fw-bold fs-5 mt-auto">
-                            $${producto.precio}
+                            UDS ${producto.precio}
                         </p>
 
                         <p class="mb-0">
